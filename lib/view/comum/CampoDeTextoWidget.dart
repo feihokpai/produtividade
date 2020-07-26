a736aab38c9e8a2cc6e0b82eb58e5cd9ba0112f8
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2020-07-26 08:37:50 -0300

    Fiz uma reforma na classe CampoDeTextoWidget, pra que receba uma key.

--- lib/view/comum/CampoDeTextoWidget.dart
+++ lib/view/comum/CampoDeTextoWidget.dart
@@ -7,14 +7,29 @@ class CampoDeTextoWidget{
   String _labelCampo;
   TextStyle _textStyleLabel = Estilos.textStyleLabelTextFormField;
   TextStyle _textStyleTexto = Estilos.textStyleListaPaginaInicial;
+  TextFormField _widget;
+  Key _key;
   int _linhas;
 
   String Function(String) funcaoValidacao;
 
-  CampoDeTextoWidget( String label, int qtdLinhas, String Function(String) validacao ){
+  CampoDeTextoWidget( String label, int qtdLinhas, String Function(String) validacao, {Key chave} ){
     this.linhas = qtdLinhas;
     this.labelCampo = label;
     this.funcaoValidacao = validacao;
+    if( chave != null){
+      this._key = chave;
+    }else{
+      this._key = UniqueKey();
+    }
+  }
+
+  void setKeyString(String valor){
+    if( this._widget != null ){
+      throw new Exception( "Não pode setar a key depois que o widget já foi criado. Somente use setKeyString()"
+          " se não tiver atribuído o valor no construtor ou não tiver chamado este mesmo método para o mesmo objeto antes." );
+    }
+    this._key = new ValueKey( valor );
   }
 
   String getText(){
@@ -44,18 +59,27 @@ class CampoDeTextoWidget{
   String get labelCampo => ( _labelCampo ?? "" );
   void set labelCampo(String valor) => this._labelCampo = valor;
 
+  TextFormField get widget{
+    if( this._widget == null ){
+      this._widget = new TextFormField(
+        key: this._key,
+        keyboardType: TextInputType.text,
+        decoration: new InputDecoration(
+            labelText: labelCampo,
+            labelStyle: this.textStyleLabel,
+            border: new OutlineInputBorder()
+        ),
+        style: this.textStyleTexto,
+        controller: this.campoController,
+        validator: this.funcaoValidacao,
+        maxLines: this.linhas,
+      );
+    }
+    return this._widget;
+  }
+
   Widget getWidget(){
-    return new TextFormField( keyboardType: TextInputType.text
-      , decoration: new InputDecoration(
-          labelText: labelCampo,
-          labelStyle: this.textStyleLabel,
-          border: new OutlineInputBorder()
-      ),
-      style: this.textStyleTexto,
-      controller: this.campoController,
-      validator: this.funcaoValidacao,
-      maxLines: this.linhas,
-    );
+    return this.widget;
   }
 
 }
