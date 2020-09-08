e612334e87ac33b633f0f8b23aa6269db30a303b
Fabiano Amorim <fabiano.silva.amorim@gmail.com>
2020-09-08 05:57:12 -0300

    * It was changed the way how the Timers are stored in TimersProdutividade. Now the identifier in the map is a Statefull Object. Before was a Type.

--- lib/view/comum/TimersProdutividade.dart
+++ lib/view/comum/TimersProdutividade.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
 /// This class has the objective to have only one active periodic Timer to refresh each screen.
 class TimersProdutividade{
-//  static Map<Type, _TimerProdutividade> _timers = new Map();
-  static Map<Type, Timer> _timers = new Map();
-  static Map<Type, int> callsNumber = new Map();
+  static Map<StatefulWidget, Timer> _timers = new Map();
+  static Map<StatefulWidget, int> callsNumber = new Map();
   static bool printLogs = false;
 
   ///     Verify if exists some Timer associated to "runtime type" of the widget in parameter. If don't exist,
@@ -21,16 +19,20 @@ class TimersProdutividade{
     _setTimer(widget, frequencyInMiliseconds, operation );
   }
 
+  static String _widgetIdentifier( StatefulWidget widget ){
+    return "${widget.runtimeType}_${widget.hashCode}";
+  }
+
   static void cancelTimerIfActivated( StatefulWidget widget ){
     Timer timer = _getActiveTimer(widget);
     if( timer != null ){
       timer.cancel();
-      _printLog( "Timer of ${widget.runtimeType} canceled" );
+      _printLog( "Timer of ${_widgetIdentifier(widget)} canceled" );
     }
   }
 
   static void cancelAllTimers(){
-    _timers.forEach( (Type type, Timer timer) {
+    _timers.forEach( ( mapKey, Timer timer) {
       timer.cancel();
     });
   }
@@ -44,7 +46,7 @@ class TimersProdutividade{
   }
 
   static Timer _getTimer(StatefulWidget widget){
-    return _timers[widget.runtimeType];
+    return _timers[widget];
   }
 
   static void _setTimer(StatefulWidget widget, int frequencyMili, void Function( ) operation ){
@@ -54,19 +56,19 @@ class TimersProdutividade{
     }
     Duration frequency = new Duration( milliseconds: frequencyMili );
     _initCallNumber( widget );
-    _timers[widget.runtimeType] = Timer.periodic( frequency, (timer){
-      _printLog( "Timer of ${widget.runtimeType} executed: ${_nextCallNumber(widget)}" );
+    _timers[widget] = Timer.periodic( frequency, (timer){
+      _printLog( "Timer of ${_widgetIdentifier(widget)} executed: ${_nextCallNumber(widget)}" );
       operation.call();
     } );
-    _printLog( "Timer of ${widget.runtimeType} created" );
+    _printLog( "Timer of ${_widgetIdentifier(widget)} created" );
   }
 
   static void _initCallNumber( StatefulWidget widget ){
-    callsNumber[widget.runtimeType] = 0;
+    callsNumber[widget] = 0;
   }
 
   static int _nextCallNumber( StatefulWidget widget ){
-    return (++callsNumber[widget.runtimeType]);
+    return (++callsNumber[widget]);
   }
 
   static void _printLog(String msg){
